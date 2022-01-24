26a539a839d13b73cb727b4112a86d567fe0c197
Oliver Bartsch <bo@cedev.de>
2022-01-24 11:20:14 +0100

    [TASK] Move global page TSconfig to dedicated file

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3') or die();
-
-ExtensionManagementUtility::addPageTSConfig(
-    'mod.web_info.fieldDefinitions.2.fields := addToList(fe_login_mode)'
-);
 
 $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',fe_login_mode';
